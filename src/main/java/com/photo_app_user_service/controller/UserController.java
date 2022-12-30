0d7c08d5846b4297242ae29753fa0331bfed5e7b
bsyso <bsyso@softserveinc.com>
2022-12-30 09:34:57 +0100

    added controller

---
+++ src/main/java/com/photo_app_user_service/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.photo_app_user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Working...";
+    }
+}
