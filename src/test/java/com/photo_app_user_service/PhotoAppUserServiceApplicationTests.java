d5f0da300baf659629a8bfbaf414a9df74cd3495
bsyso <bsyso@softserveinc.com>
2022-12-30 09:24:06 +0100

    Initial commit

---
+++ src/test/java/com/photo_app_user_service/PhotoAppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo_app_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
